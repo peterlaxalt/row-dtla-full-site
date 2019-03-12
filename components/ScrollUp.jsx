82dbe4e63b2a4547b2c3e2022eff0f38c24aa68c
Spencer Fink <fink.spencer@gmail.com>
2019-03-12 18:54:32 -0400

    adds scroll up button

---
+++ components/ScrollUp.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ScrollUpButton = styled.div`
+  &::before {
+    content: '\\f139';
+    font: normal normal normal 14px/1 FontAwesome;
+    font-size: 32px;
+  }
+  position: fixed;
+  cursor: pointer;
+  color: #000;
+  bottom: 1%;
+  right: 1%;
+  ${props =>
+    props.showButton
+      ? 'opacity: 1; visibility: visible;'
+      : 'opacity: 0; visibility: hidden;'}
+  transition: 0.25s all ease-in-out;
+`;
+
+export default class ScrollUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showButton: false
+    };
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollHandler);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollHandler);
+  }
+  scrollHandler = () => {
+    if (window.pageYOffset > 150 && !this.state.showButton) {
+      this.setState({
+        showButton: true
+      });
+    }
+    if (window.pageYOffset <= 150 && this.state.showButton) {
+      this.setState({
+        showButton: false
+      });
+    }
+  };
+  scrollStep = () => {
+    if (window.pageYOffset === 0) {
+      clearInterval(this.state.intervalId);
+    }
+    window.scroll(0, window.pageYOffset - 75);
+  };
+
+  scrollToTop = () => {
+    let intervalId = setInterval(this.scrollStep, 10);
+    this.setState({ intervalId: intervalId });
+  };
+
+  render() {
+    return (
+      <ScrollUpButton
+        showButton={this.state.showButton}
+        onClick={this.scrollToTop}
+      />
+    );
+  }
+}
--- pages/buildings.js
+++ pages/buildings.js
@@ -2,6 +2,7 @@ import Layout from '../components/layouts/default.js';
 import ImageSlider from '../components/Slider';
 import ImageFader from '../components/Fader';
 import BeforeAfter from '../components/BeforeAfter';
+import ScrollUp from '../components/ScrollUp';
 import { testArray, testBefore, testAfter } from '../data/test.js';
 
 const BuildingsPage = () => (
@@ -10,6 +11,7 @@ const BuildingsPage = () => (
     <ImageFader imgArray={testArray} autoPlay={true} />
     <ImageSlider imgArray={testArray} showQuotes={true} autoPlay={true} />
     <BeforeAfter before={testBefore} after={testAfter} />
+    <ScrollUp />
   </Layout>
 );
 
