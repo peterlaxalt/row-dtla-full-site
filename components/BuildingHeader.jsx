4215d9a157b33a5a307070cc820d0a34c5a81884
Spencer Fink <fink.spencer@gmail.com>
2019-03-14 18:24:12 -0400

    adds mobile styles to building header

--- components/BuildingHeader.jsx
+++ components/BuildingHeader.jsx
@@ -11,6 +11,9 @@ const HeaderContainer = styled.div`
   background-color: #fff;
   height: 1450px;
   width: 100%;
+  @media screen and (max-width: 1024px){
+      height: 420px;
+  }
 `;
 
 const LogoContainer = styled.div`
@@ -20,6 +23,9 @@ const LogoContainer = styled.div`
   padding: 300px 0;
   z-index: 1;
   text-align: center;
+  @media screen and (max-width: 1024px) {
+    padding: 0;
+  }
 `;
 
 const HeaderLogo = styled.div`
@@ -33,6 +39,13 @@ const HeaderLogo = styled.div`
   img {
     max-height: 100%;
     max-width: 100%;
+  }
+  @media screen and (max-width: 1024px) {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 75%;
   }
 `;
 
