a14fb890c15af3bc940cc267e12249bf79f6f0c9
Spencer Fink <fink.spencer@gmail.com>
2019-03-15 15:28:53 -0400

    Adds Fetching to the Context Provider

--- provider/ContextProvider.jsx
+++ provider/ContextProvider.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Context from '../config/Context';
+import $ from 'jquery';
 
 class ContextProvider extends Component {
   constructor(props) {
@@ -8,11 +9,17 @@ class ContextProvider extends Component {
       windowDimensions: {
         height: null,
         width: null
-      }
+      },
+      availabilityData: [],
+      pressData: [],
+      newsData: []
     };
   }
   componentDidMount() {
     this.updateWindowDimensions();
+    this.fetchAvailability();
+    this.fetchNews();
+    this.fetchPress();
     window.addEventListener('resize', this.updateWindowDimensions);
   }
 
@@ -26,6 +33,54 @@ class ContextProvider extends Component {
         height: window.innerHeight,
         width: window.innerWidth
       }
+    });
+  };
+
+  fetchAvailability = () => {
+    $.ajax({
+      url: 'https://cms.dbox.com/wp-json/wp/v2/hsp_availability'
+    }).then(data => {
+      data = data.map((el, idx) => {
+        return el.acf;
+      });
+      this.setState({
+        availabilityData: data
+      });
+      console.log(data);
+    });
+  };
+
+  fetchPress = () => {
+    $.ajax({
+      url: 'https://cms.dbox.com/wp-json/wp/v2/hsp_press'
+    }).then(data => {
+      data = data.map((el, idx) => {
+        return el.acf;
+      });
+      data = data.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+      this.setState({
+        pressData: data
+      });
+      console.log(data);
+    });
+  };
+
+  fetchNews = () => {
+    $.ajax({
+      url: 'https://cms.dbox.com/wp-json/wp/v2/hsp_news'
+    }).then(data => {
+      data = data.map((el, idx) => {
+        return el.acf;
+      });
+      data = data.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+      });
+      this.setState({
+        newsData: data
+      });
+      console.log(data);
     });
   };
 
