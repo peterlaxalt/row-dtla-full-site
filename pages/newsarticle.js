cf627e89ce3ca96d33d51f11e6071193fa9fce69
Spencer Fink <fink.spencer@gmail.com>
2019-03-20 19:19:06 -0400

    news article progress

--- pages/newsarticle.js
+++ pages/newsarticle.js
@@ -3,7 +3,7 @@ import Layout from '../components/layouts/default';
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
-import Slider from '../components/Slider';
+import ImageSlider from '../components/Slider';
 import Context from '../config/Context';
 
 const BackButtonInnner = styled.a`
@@ -32,33 +32,60 @@ const BackButton = () => {
   );
 };
 
-const getCurrentData = (allNewsItems, title) => {
-  for (let i = 0; i < allNewsItems.length; i++) {
-    if (allNewsItems[i].title == title) {
-      return allNewsItems[i];
+class NewsArticle extends React.Component {
+  getCurrentData = (allNewsItems, title) => {
+    for (let i = 0; i < allNewsItems.length; i++) {
+      if (allNewsItems[i].title == title) {
+        return allNewsItems[i];
+      }
     }
+  };
+
+  createImageArray = articleData => {
+    let imageArray = [];
+    if (articleData.image_1.url) {
+      imageArray.push({ imgUrl: articleData.image_1.url });
+    }
+    if (articleData.image_2.url) {
+      imageArray.push({ imgUrl: articleData.image_2.url });
+    }
+    if (articleData.image_3.url) {
+      imageArray.push({ imgUrl: articleData.image_3.url });
+    }
+    if (articleData.image_4.url) {
+      imageArray.push({ imgUrl: articleData.image_4.url });
+    }
+    if (articleData.image_5.url) {
+      imageArray.push({ imgUrl: articleData.image_5.url });
+    }
+    return imageArray;
+  };
+
+  render() {
+    return (
+      <Context.Consumer>
+        {context => {
+          console.log(context);
+          let articleData = this.getCurrentData(
+            context.state.newsData,
+            this.props.router.query.title
+          );
+          let imageArray = this.createImageArray(articleData);
+          console.log(imageArray);
+          return (
+            <Layout>
+              <BackButton />
+              <ImageSlider
+                imgArray={imageArray}
+                showQuotes={true}
+                autoPlay={true}
+              />
+              <h1>{articleData.title}</h1>
+            </Layout>
+          );
+        }}
+      </Context.Consumer>
+    );
   }
-};
-
-const NewsArticle = props => {
-  console.log(props);
-  return (
-    <Context.Provider>
-      {context => (
-        <Layout>
-          <BackButton />
-          <h1>{props.router.query.title}</h1>
-        </Layout>
-      )}
-    </Context.Provider>
-  );
-};
-
-// NewsArticle.getInitialProps = async function (context) {
-//   const { slug } = context.query;
-//   const article = buildings.find(building => building.slug === slug);
-
-//   return { building };
-// };
-
+}
 export default withRouter(NewsArticle);
