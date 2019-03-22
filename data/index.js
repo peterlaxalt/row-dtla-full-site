187c7f3071075999f078c1a6da163d5b92dfe96f
Spencer Fink <fink.spencer@gmail.com>
2019-03-22 19:01:10 -0400

    begins work on clickslider for index and buildings

--- data/index.js
+++ data/index.js
@@ -1,38 +1,4 @@
-export const testArray = [
-  {
-    imgUrl: '/static/images/test/building1.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building2.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building1.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building2.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building1.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building2.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building1.jpg',
-    imgAlt: 'TEST ALT'
-  },
-  {
-    imgUrl: '/static/images/test/building2.jpg',
-    imgAlt: 'TEST ALT'
-  }
-];
-export const testTitleArray = [
+export const indexArray = [
   {
     imgUrl: '/static/images/test/building1.jpg',
     imgAlt: 'TEST ALT',
@@ -82,11 +48,3 @@ export const testTitleArray = [
     link: '/buildings/100-avenue-of-the-americas/'
   }
 ];
-export const testBefore = {
-  path: '/static/images/test/building2.jpg',
-  alt: 'TEST1'
-};
-export const testAfter = {
-  path: '/static/images/test/building3.jpg',
-  alt: 'TEST2'
-};
--- pages/buildings.js
+++ pages/buildings.js
@@ -1,15 +1,20 @@
-import ImageSlider from '../components/Slider';
+import ClickSlider from '../components/ClickSlider';
 import ImageFader from '../components/Fader';
 import BeforeAfter from '../components/BeforeAfter';
 import ScrollUp from '../components/ScrollUp';
 import BrandSpinner from '../components/BrandSpinner';
-import { testArray, testBefore, testAfter } from '../data/test.js';
+import {
+  testArray,
+  testTitleArray,
+  testBefore,
+  testAfter
+} from '../data/test.js';
 
 const Buildings = () => (
   <div>
     <p>Buildings Next.js</p>
+    <ClickSlider imgArray={testTitleArray} index={true} autoPlay={true} />
     <ImageFader imgArray={testArray} autoPlay={true} />
-    <ImageSlider imgArray={testArray} showQuotes={true} autoPlay={true} />
     <BeforeAfter before={testBefore} after={testAfter} />
     <BrandSpinner />
     <ScrollUp />
