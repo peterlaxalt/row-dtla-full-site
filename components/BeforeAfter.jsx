84f285d19a9ffe7443a9a252658e73a781692689
Spencer Fink <fink.spencer@gmail.com>
2019-03-26 18:55:06 -0400

    Makes before after responsive and adds aligned images

--- components/BeforeAfter.jsx
+++ components/BeforeAfter.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import TwentyTwenty from 'react-twentytwenty';
 import styled from 'styled-components';
+import ResponsiveImage from './ResponsiveImage';
 
 const BeforeAfterContainer = styled.div`
   height: 90vh;
-  & > div {
+  div {
     height: 100%;
+  }
+  .responsive-image {
+    min-width: 100%;
+    min-height: 100%;
   }
 `;
 
@@ -28,8 +33,8 @@ export default class BeforeAfter extends React.Component {
     return (
       <BeforeAfterContainer>
         <TwentyTwenty
-          left={<img src={before.path} alt={before.alt} />}
-          right={<img src={after.path} alt={after.alt} />}
+          left={<ResponsiveImage srcPath={before.path} imgAlt={before.alt} />}
+          right={<ResponsiveImage srcPath={after.path} imgAlt={after.alt} />}
           slider={
             <SliderIconContainer>
               <Slider src="/static/images/test/close.svg" alt="slider handle" />
--- pages/story.js
+++ pages/story.js
@@ -1,6 +1,52 @@
+import styled from 'styled-components';
+import ImageSlider from '../components/Slider';
+import BeforeAfter from '../components/BeforeAfter';
+import ScrollUp from '../components/ScrollUp';
+import CopyrightFooter from '../components/CopyrightFooter';
+import Context from '../config/Context';
+import ResponsiveImage from '../components/ResponsiveImage';
+
+const beforeAfterOne = {
+  before: {
+    path: '/static/images/story/1_Slider/1_Aerial_Now_Aligned',
+    alt: ''
+  },
+  after: {
+    path: '/static/images/story/1_Slider/1_Aerial_Then_Aligned',
+    alt: ''
+  }
+};
+
+const beforeAfterTwo = {
+  before: {
+    path: '',
+    alt: ''
+  },
+  after: {
+    path: '',
+    alt: ''
+  }
+};
+
+const Heading = styled.h2`
+  font-size: 34px;
+  font-weight: 700;
+  line-height: 44px;
+  letter-spacing: 1px;
+  text-align: center;
+`;
+
 const Story = () => (
   <div>
-    <p>Story Next.js</p>
+    <Heading>
+      Hudson Square Properties is a joint venture of Trinity Church Wall Street
+      and Norges Bank Real Estate Management with Hines, the operating partner,
+      of a 12 building portfolio of approximately 6 million rentable square
+      feet.
+    </Heading>
+    <BeforeAfter before={beforeAfterOne.before} after={beforeAfterOne.after} />
+    <ScrollUp />
+    <CopyrightFooter />
   </div>
 );
 
