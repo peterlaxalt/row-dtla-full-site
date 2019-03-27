6794247848eb6a464845b9416266af29ece5df21
Joel Hoelting <joelhoelting@protonmail.com>
2019-03-27 17:41:40 -0400

    Fix building Hero image names, remove extraneous image files, add building data

--- components/layouts/default.js
+++ components/layouts/default.js
@@ -2,14 +2,16 @@ import Head from 'next/head';
 import Header from '../includes/Header';
 import { withRouter } from 'next/router';
 
+import { capitalizeFirstLetter } from '~/helpers/strings';
+
 const Layout = (props) => {
   const title = props.router.pathname.replace('/', '');
-
+  
   return (
     <React.Fragment>
       <Head>
         <title>
-          {title ? `${title} - Hudson Square Properties` : 'Hudson Square Properties'}
+          {title ? `${capitalizeFirstLetter(title)} - Hudson Square Properties` : 'Hudson Square Properties'}
         </title>
       </Head>
       <Header />
