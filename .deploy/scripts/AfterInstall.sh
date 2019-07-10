2427aafb5dd382a968c91b26c5232ccfa148220d
Joel Hoelting <joelhoelting@protonmail.com>
2019-07-10 13:22:43 -0400

    CodeDeploy agent make script relative to application directory

--- .deploy/scripts/AfterInstall.sh
+++ .deploy/scripts/AfterInstall.sh
@@ -2,6 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #
 
+cd /home/ubuntu/Dev/hsp-full-site-next
 npm install
 npm run build
-pm2 start "hsp-full-site-next" || pm2 start npm --name "hsp-full-site-next" -- start
+pm2 start npm --name "hsp-full-site-next" -- start
