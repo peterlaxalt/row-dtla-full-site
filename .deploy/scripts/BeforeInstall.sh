460592a56d54dc0f4d24b96cee0454d11794d244
Joel Hoelting <joelhoelting@protonmail.com>
2019-07-10 17:58:30 -0400

    CI/CD - Update BeforeInstall script to delete folder prior to install event

--- .deploy/scripts/BeforeInstall.sh
+++ .deploy/scripts/BeforeInstall.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -d /home/ubuntu/Dev/hsp-full-site-next ]; then
+  echo "Directory exists: deleting directory..."
+  rm -rf /home/ubuntu/Dev/hsp-full-site-next
+fi
+
 pm2 kill || true
