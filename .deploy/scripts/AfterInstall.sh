5cc24b332fa03d2752f5e224c9fe24a0a5980786
Joel Hoelting <joelhoelting@protonmail.com>
2019-07-10 16:47:54 -0400

    Deployment Fixes: CodeDeploy script test

--- .deploy/scripts/AfterInstall.sh
+++ .deploy/scripts/AfterInstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #
 
 cd /home/ubuntu/Dev/hsp-full-site-next
 npm install
--- .deploy/scripts/BeforeInstall.sh
+++ .deploy/scripts/BeforeInstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #
 
 pm2 kill || true
