3206f93586e10e6670ecc3a7ce5cca327e04e1b1
Joel Hoelting <joelhoelting@protonmail.com>
2019-07-10 17:23:35 -0400

    CI/CD - Deletes package-lock.json and attempts to run build script

--- .deploy/scripts/AfterInstall.sh
+++ .deploy/scripts/AfterInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 cd /home/ubuntu/Dev/hsp-full-site-next
+rm package-lock.json
+echo "Package Lock File Deleted"
 npm install
-pm2 start --name "hsp-full-site-next" npm --run dev
+echo "Installed NPM Packages"
+npm run build
+pm2 start npm --name "hsp-full-site-next" -- start
