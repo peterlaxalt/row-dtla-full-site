4512c9e153195e75652f91d3db6ad69f3644f36b
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-21 18:42:49 -0400

    Update blogposts.js to use as an example, remove blogpost template

--- src/pages/blogposts.js
+++ src/pages/blogposts.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Layout from '~/components/layouts';
+import SEO from '~/components/seo';
 
 const BlogPosts = ({ data }) => {
   const blogPosts = data.allContentfulBlogPost.edges;
