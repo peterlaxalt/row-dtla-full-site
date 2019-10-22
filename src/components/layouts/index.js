f257e1d8b1f4627d249dd877cc5204b094967bfe
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-22 11:29:42 -0400

    Ensure height is 100% in all parent divs

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -28,14 +28,7 @@ const Layout = ({ children }) => {
     <>
       <GlobalStyles />
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div className="container">
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
--- src/styles/Global.js
+++ src/styles/Global.js
@@ -3,8 +3,23 @@ import { Global, css } from '@emotion/core';
 const GlobalStyles = () => (
   <Global
     styles={css`
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body,
+      #___gatsby {
+        height: 100%;
+      }
+
       body {
-        background-color: grey;
+        margin: 0;
+        padding: 0;
+      }
+
+      div[role='group'][tabindex] {
+        height: 100%;
       }
     `}
   />
