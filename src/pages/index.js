c7d2820d7289b94d89234d7b2a443bb3d30c8b23
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-22 14:42:13 -0400

    Fix filename errors, strip away extraneous code in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,11 @@ import Layout from '~/components/layouts';
 import Image from '~/components/image';
 import SEO from '~/components/seo';
 
-import styled from '@emotion/styled';
-import { minWidth } from '~/styles/mediaQueries';
-
-const TestDiv = styled.div`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  color: black;
-  font-weight: bold;
-  ${minWidth('phone')} {
-    font-size: 240px;
-  },
-`;
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div className="home">
-      <h1>Hello There</h1>
-      <p>Welcome my awesome blog</p>
+      <h1>ROW DTLA</h1>
       <div>
         <div
           style={{
@@ -34,7 +18,6 @@ const IndexPage = () => (
           }}
         >
           <Image />
-          <TestDiv>Hello There</TestDiv>
         </div>
       </div>
       <Link to="/blogposts/">View all posts</Link>
