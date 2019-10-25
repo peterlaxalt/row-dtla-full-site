331321c529678a355ba1c993598cc38ffd234dc6
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-25 17:46:41 -0400

    Add rough collective item page

--- src/templates/collectiveShow.js
+++ src/templates/collectiveShow.js
@@ -1,15 +1,19 @@
 import React from 'react';
-// import { Link, graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Layout from '~/components/layouts';
 import SEO from '~/components/seo';
 
+import collectiveItem from '~/data/local/collectiveItem';
+
 const CollectiveShow = ({ data }) => {
-  const { title } = data.contentfulCollectiveItem;
+  // const { title } = data.contentfulCollectiveItem;
+  const { descriptionBody, title } = collectiveItem;
 
   return (
     <Layout>
       <SEO title={title} />
       <h1>{title}</h1>
+      <p>{descriptionBody.descriptionBody}</p>
       <Link to="/">Back to Home</Link>
     </Layout>
   );
@@ -17,41 +21,39 @@ const CollectiveShow = ({ data }) => {
 
 export default CollectiveShow;
 
-// export const pageQuery = graphql`
-//   query($slug: String!) {
-//     contentfulCollectiveItem(slug: {eq: "13-bonaparte-5"}) {
-//     id
-//     displayTitle {
-//       displayTitle
-//     }
-//     addressLine1
-//     addressLine2
-//     descriptionBody {
-//       descriptionBody
-//     }
-//     parking
-//     instagram
-//     timeCloseFriday
-//     timeCloseMonday
-//     timeCloseSaturday
-//     timeCloseSunday
-//     timeOpenWednesday
-//     timeOpenTuesday
-//     timeOpenThursday
-//     timeOpenSunday
-//     timeOpenSaturday
-//     timeOpenMonday
-//     timeOpenFriday
-//     timeCloseWednesday
-//     timeCloseTuesday
-//     timeCloseThursday
-//     subtitle
-//     seoDescription {
-//       seoDescription
-//     }
-//     email
-//     facebook
-//     website
-//   }
-//   }
-// `;
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulCollectiveItem(slug: { eq: $slug }) {
+      id
+      addressLine1
+      addressLine2
+      descriptionBody {
+        descriptionBody
+      }
+      parking
+      instagram
+      timeCloseFriday
+      timeCloseMonday
+      timeCloseSaturday
+      timeCloseSunday
+      timeOpenWednesday
+      timeOpenTuesday
+      timeOpenThursday
+      timeOpenSunday
+      timeOpenSaturday
+      timeOpenMonday
+      timeOpenFriday
+      timeCloseWednesday
+      timeCloseTuesday
+      timeCloseThursday
+      subtitle
+      seoDescription {
+        seoDescription
+      }
+      email
+      facebook
+      website
+      title
+    }
+  }
+`;
