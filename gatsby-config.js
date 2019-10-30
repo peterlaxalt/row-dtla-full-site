2d9f2bf6d83f6f40086d828c5d3f86e9748cf70f
Joel Hoelting <joelhoelting@protonmail.com>
2019-10-30 11:39:56 -0400

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Row DTLA`,
+        short_name: `Row`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon/row_favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
