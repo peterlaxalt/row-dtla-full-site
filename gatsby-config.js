37ac85fca66501346cdfe2d98b121291106f608a
Joel Hoelting <joelhoelting@protonmail.com>
2019-12-04 17:13:49 -0500

    Adds google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: `ROW DTLA`,
     description: `ROW DTLA’s entrepreneurial innovators who are pushing LA culture forward with concepts designed to make a global impact.`,
-    author: `YOUR_NAME`,
+    author: `YOUR_NAME`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,8 +12,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
+        path: `${__dirname}/src/assets/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -27,31 +27,37 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/favicon/row_favicon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/assets/images/favicon/row_favicon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve(`./src/layouts`),
-      },
+        component: require.resolve(`./src/layouts`)
+      }
     },
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
-        '~': path.join(__dirname, 'src/'),
-      },
+        '~': path.join(__dirname, 'src/')
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `v5y5potka9e9`,
-        accessToken: `U4Qe0URgCsIQHgoRvVnjwLRAc5auF9sC-GSidrRi86Y`,
+        accessToken: `U4Qe0URgCsIQHgoRvVnjwLRAc5auF9sC-GSidrRi86Y`
         // environment: `develop`,
-      },
+      }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-153263532-2'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 };
