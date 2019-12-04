1cbc3902cb6a15759404285943e9a77cb7090c91
Joel Hoelting <joelhoelting@protonmail.com>
2019-12-04 18:19:12 -0500

    Updates travis script (travis.yml)

--- clear_cf_cache.sh
+++ clear_cf_cache.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Hello World"
-echo $TRAVIS_BRANCH
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  travis-ci-cloudfront-invalidation -a $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -c $AWS_CLOUDFRONT_DIST_ID -i '/*' -b $TRAVIS_BRANCH -p $TRAVIS_PULL_REQUEST
+else
+  echo "Skipping Cloudfront Invalidation on branch $TRAVIS_BRANCH"
+fi
