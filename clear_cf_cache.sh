b144f3a8b1048dae85a1910095b88f8f199554f4
Joel Hoelting <joelhoelting@protonmail.com>
2019-12-10 15:14:52 -0500

    Adds CF Distribution echo in post deploy script

--- clear_cf_cache.sh
+++ clear_cf_cache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
+  echo "Invalidating cache for CF Distribution: $AWS_CLOUDFRONT_DIST_ID"
   travis-ci-cloudfront-invalidation -a $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -c $AWS_CLOUDFRONT_DIST_ID -i '/*' -b $TRAVIS_BRANCH -p $TRAVIS_PULL_REQUEST
 else
   echo "Skipping Cloudfront Invalidation on branch $TRAVIS_BRANCH"
